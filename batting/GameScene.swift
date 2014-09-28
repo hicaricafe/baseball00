891d2838d893893f35024c8db1a265b32aa3a744
hikaricafe <hikaricafe@gmail.com>
2014-09-28 10:01:54 +0900

    baseball

--- batting/GameScene.swift
+++ batting/GameScene.swift
@@ -6,18 +6,14 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     var batter = SKSpriteNode()
     var pitcher = SKSpriteNode()
     var ball = SKSpriteNode()
-    var shootbtn = SKSpriteNode()
     var maru = SKSpriteNode()
-    var bat = SKSpriteNode()
     
     var last:CFTimeInterval!
     
     let ballCategory:       UInt32 = 0x1 << 0
     let maruCategory:       UInt32 = 0x1 << 1
     let pitcherCategory:    UInt32 = 0x1 << 2
-    let batterCategory:     UInt32 = 0x1 << 3
-    let groundCategory:     UInt32 = 0x1 << 4
-    
+
 
     override func didMoveToView(view: SKView) {
 
@@ -25,43 +21,16 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         self.physicsWorld.contactDelegate = self
     
         
-
-        //batter
-
-        //batterTexture.filteringMode = SKTextureFilteringMode.Nearest
-    
-        //batter = SKSpriteNode(texture: batterTexture1)
-        
-
-        
-//        batter.setScale(1.0)
-//        batter.position = CGPointMake(self.frame.size.width * 0.6, self.frame.size.height * 0.6 )
-//
-//        batter.physicsBody = SKPhysicsBody(circleOfRadius: batter.size.height/2.0)
-//        batter.physicsBody?.dynamic = true
-//        batter.physicsBody?.allowsRotation = false
-//
-////        batter.physicsBody?.categoryBitMask = batterCategory
-////        batter.physicsBody?.contactTestBitMask = ballCategory
-        
-  //      self.addChild(batter)
-        
-
-        
-        
-
-        
-        
         //pitcher
         var pitcherTexture = SKTexture(imageNamed: "pitcher")
         pitcherTexture.filteringMode = SKTextureFilteringMode.Nearest
         
         pitcher = SKSpriteNode(texture: pitcherTexture)
         pitcher.setScale(1.0)
-        pitcher.position = CGPointMake(self.frame.size.width * 0.05, self.frame.size.height * 0.6 )
+        pitcher.position = CGPointMake(self.frame.size.width * 0.05, self.frame.size.height * 0.5 )
         
         pitcher.physicsBody = SKPhysicsBody(circleOfRadius: pitcher.size.height/2.0)
-        pitcher.physicsBody?.dynamic = true
+        pitcher.physicsBody?.dynamic = false
         pitcher.physicsBody?.allowsRotation = false
         pitcher.physicsBody?.friction = 1.0
         pitcher.physicsBody?.mass = 1000000000.0
@@ -71,30 +40,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         
         self.addChild(pitcher)
         
-        //Ground
-        
-        var groundTexture = SKTexture(imageNamed: "ground")
-        
-        var ground = SKSpriteNode(texture: groundTexture)
-        ground.setScale(1.0)
-        ground.position = CGPointMake(self.size.width/2, ground.size.height/2)
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(groundTexture.size().width, groundTexture.size().height))
-        ground.physicsBody?.dynamic = false
-        
-        ground.physicsBody?.categoryBitMask = groundCategory
-        
-        self.addChild(ground)
-        
-        
-        //shootbtn
-        var ShootbtnTexture = SKTexture(imageNamed: "shoot")
-        
-        var shootbtn = SKSpriteNode(texture: ShootbtnTexture)
-        shootbtn.setScale(1.0)
-        shootbtn.position = CGPointMake(self.size.width/2 - 100, 150)
-        shootbtn.name = "shoot"
-        
-        self.addChild(shootbtn)
+
         
         //battingbtn
         var battingbtnTexture = SKTexture(imageNamed: "batting")
@@ -113,56 +59,32 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     }
     
     func shoot() {
-        
-        //self.runAction(SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false))
+    
         
         var BallTexture = SKTexture(imageNamed: "ball")
         ball = SKSpriteNode(texture: BallTexture)
         ball.position = CGPointMake(self.pitcher.position.x + 50, self.pitcher.position.y )
 
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.height/2.0)
-        
         ball.physicsBody?.mass = 100.0
-        ball.physicsBody?.affectedByGravity = true
+        ball.physicsBody?.affectedByGravity = false
         ball.physicsBody?.dynamic = true
-        
-
-
         self .addChild(ball)
-    
-        ball.physicsBody?.applyImpulse(CGVectorMake(150000, 10000))
+
+        ball.physicsBody?.applyImpulse(CGVectorMake(150000, 0))
         ball.physicsBody?.restitution = 1.5
-        
         ball.physicsBody?.categoryBitMask = ballCategory
         ball.physicsBody?.contactTestBitMask = maruCategory
-
     }
     
-    
-    
     func hit() {
-        
-        //self.runAction(SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false))
-        
 
-        
         var maruTexture = SKTexture(imageNamed: "maru")
         maru = SKSpriteNode(texture: maruTexture)
-        maru.position = CGPointMake(self.size.width/2 + 400, self.pitcher.position.y - 5 )
-
-        
+        maru.position = CGPointMake(self.size.width/2 + 400, self.pitcher.position.y  )
         maru.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(maruTexture.size().width, maruTexture.size().height))
-        
-        
         maru.physicsBody?.dynamic = false
         maru.physicsBody?.allowsRotation = false
-        
-        let pi:CGFloat = 3.1415926
-        maru.runAction(SKAction.rotateByAngle(pi * 1.0, duration: 0.05))
-
-        
-//        maru.physicsBody = SKPhysicsBody(circleOfRadius: maru.size.height/2.0)
-//        maru.physicsBody?.dynamic = false
 
         self .addChild(maru)
         
@@ -176,7 +98,6 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
             let location = touch.locationInNode(self)
             
             if nodeAtPoint(location).name == "shoot" {
-          //      println("shoot")
                 shoot()
             }
         
@@ -190,9 +111,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
                       println("maru")
                 hit()
             }
-            
         }
-        
     }
    
     override func update(currentTime: CFTimeInterval) {
@@ -201,7 +120,6 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
             last = currentTime
         }
         
-        // 1秒おきに行う処理をかく。
         if last + 1 <= currentTime {
             
             self.shoot()
@@ -218,11 +136,8 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
       
         
     }
-    
 
-    
     func dakyu(){
-        println("dakyu")
         ball.runAction(SKAction .moveTo(CGPointMake(100,600), duration:0.5))
         
     }
@@ -243,16 +158,9 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         if firstBody.categoryBitMask & ballCategory != 0 &&
             secondBody.categoryBitMask & maruCategory != 0 {
                 secondBody.node?.removeFromParent()
-                println("shoot")
-                    //self.runAction(SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false))
                 dakyu()
                 
         }
         
     }
-    
-
-    
-    
-    
 }
